4971ef6ad492bc59bf0afe79f434e65fddde9ca6
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-04-29 23:58:59 +0100

    move server initialization to api file

--- backend/modules/src/main.rs
+++ backend/modules/src/main.rs
@@ -1,15 +1,7 @@
-use actix_web::{web, App, HttpServer};
-use api::players::players::{add_player, get_player_by_id, update_player};
-use error::error::custom_json_error;
+use actix_web;
+use api::server;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-        .app_data(web::JsonConfig::default().error_handler(custom_json_error))
-        .service(web::scope("/v1/players").service(add_player).service(get_player_by_id).service(update_player))
-    })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    server::main().await
 }
