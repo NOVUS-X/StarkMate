d55466b8a8092a3da163df16645458b83f1b013c
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-04-29 23:27:01 +0100

    add new migration

--- backend/modules/db/migrations/src/lib.rs
+++ backend/modules/db/migrations/src/lib.rs
@@ -1,6 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20250428_121011_create_players_table;
+mod m20250429_193845_make_player_fields_nullable;
 
 pub struct Migrator;
 
@@ -9,6 +10,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20250428_121011_create_players_table::Migration),
+            Box::new(m20250429_193845_make_player_fields_nullable::Migration),
         ]
     }
 }
