bc21e1d3e49e9475b914e87fa2ca111fa20508c6
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-04-30 00:41:32 +0100

    add new migration

--- backend/modules/db/migrations/src/lib.rs
+++ backend/modules/db/migrations/src/lib.rs
@@ -2,6 +2,7 @@ pub use sea_orm_migration::prelude::*;
 
 mod m20250428_121011_create_players_table;
 mod m20250429_193845_make_player_fields_nullable;
+mod m20250429_231326_add_is_player_enabled;
 
 pub struct Migrator;
 
@@ -11,6 +12,7 @@ impl MigratorTrait for Migrator {
         vec![
             Box::new(m20250428_121011_create_players_table::Migration),
             Box::new(m20250429_193845_make_player_fields_nullable::Migration),
+            Box::new(m20250429_231326_add_is_player_enabled::Migration),
         ]
     }
 }
