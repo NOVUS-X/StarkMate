8bde185f88cebb9849d4b1f745d80c7f00d62e6a
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-04-30 19:43:32 +0100

    gracefully handle errors

--- backend/modules/service/src/helper/password.rs
+++ backend/modules/service/src/helper/password.rs
@@ -1,26 +1,24 @@
-use argon2::{password_hash::SaltString, Argon2, PasswordHash, PasswordHasher, PasswordVerifier};
-    
-    use rand::rngs::OsRng;
+use argon2::{Argon2, PasswordHash, PasswordHasher, PasswordVerifier, password_hash::SaltString};
 
-    pub fn hash_password(password: &str) -> String {
-        let salt = SaltString::generate(&mut OsRng);
+use rand::rngs::OsRng;
 
-        let argon2 = Argon2::default();
-        let hash = argon2.hash_password(password.as_bytes(), &salt).unwrap();
+pub fn hash_password(password: &str) -> Result<String, argon2::password_hash::Error> {
+    let salt = SaltString::generate(&mut OsRng);
+    let argon2 = Argon2::default();
+    let hash = argon2.hash_password(password.as_bytes(), &salt)?; // ? propagates errors
+    Ok(hash.to_string())
+}
 
-        hash.to_string()
+pub fn verify_password<'a>(
+    password: &'a str,
+    hashed_password: &'a str,
+) -> Result<(), argon2::password_hash::Error> {
+    let password_hash = PasswordHash::new(&hashed_password)?;
+    // Trait objects for algorithms to support
+    let algs: &[&dyn PasswordVerifier] = &[&Argon2::default()];
+
+    match password_hash.verify_password(algs, password) {
+        Ok(_) => Ok(()),
+        Err(_err) => Err(_err),
     }
-
-    pub fn verify_password<'a>(password: &'a str, hashed_password: &'a str) -> Result<(), argon2::password_hash::Error> {
-        let password_hash = PasswordHash::new(&hashed_password).expect("invalid password hash");
-
-        // Trait objects for algorithms to support
-        let algs: &[&dyn PasswordVerifier] = &[&Argon2::default()];
-
-        match password_hash.verify_password(algs, password){
-            Ok(_) => Ok(()),
-            Err(_err) => {
-                Err(_err)
-            }
-        }
-    }
+}
